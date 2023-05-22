550a7b5478acf71c5cd1d0c1ff65007d8eabf991
Marcio Marques Soares <marcio1988@hotmail.com>
2023-05-22 23:19:43 +0800

    need express to fix cors

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let chrome = require("chrome-aws-lambda");
-let puppeteer = require("puppeteer-core");
+const app = require("express")();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
-export default async function handler(req, res) {
+dotenv.config();
+
+app.get("/api", async (req, res) => {
   const url = req.query.url;
 
   if (!url) {
     return res.status(400).send("Bad Request: Please provide a URL.");
   }
+
+  if (process.env.SECRET !== req.query.secret)
+    throw new Error("Invalid secret");
 
   const browser = await puppeteer.launch({
     args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
@@ -24,4 +32,10 @@ export default async function handler(req, res) {
   const base64Image = screenshot.toString("base64");
 
   res.json({ image: base64Image });
-}
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
+
+module.exports = app;
