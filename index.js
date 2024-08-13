ff4a605a35cbf5b8245ab9f75f76600ba30d3fd6
pitieu <marcio1988@hotmail.com>
2024-08-13 13:14:33 +0800

    old code

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
 const app = require("express")();
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { chromium: playwright } = require("playwright-core");
-const chromium = require("@sparticuz/chromium");
+const chrome = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
 
 app.use(cors());
-dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+dotenv.config();
 
 app.get("/api", async (req, res) => {
   const url = req.query.url;
-  console.log(url);
+
   if (!url) {
     return res.status(400).send("Bad Request: Please provide a URL.");
   }
 
-  const browser = await playwright.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath(),
-    headless: true,
-  });
+  if (process.env.SECRET !== req.query.secret)
+    throw new Error("Invalid secret");
 
+  const browser = await puppeteer.launch({
+    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath,
+    headless: "new",
+    ignoreHTTPSErrors: true,
+  });
   const page = await browser.newPage();
-  await page.goto(url);
-
-  // Take a screenshot and store it as a buffer
-  const screenshot = await page.screenshot({
-    encoding: "base64",
-    fullPage: true,
-    animations: "allow",
-  });
-
+  await page.setViewport({ width: 1200, height: 630 });
+  await page.goto(url, { waitUntil: "networkidle0" });
+  const screenshot = await page.screenshot({ fullPage: true });
   await browser.close();
 
-  // Send the screenshot as a base64-encoded string
-  res.setHeader("Content-Type", "image/png");
-  res.send(screenshot);
+  const base64Image = screenshot.toString("base64");
+
+  res.json({ image: base64Image });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server started http://localhost:${process.env.PORT || 3000}`);
+  console.log("Server started");
 });
 
 module.exports = app;
