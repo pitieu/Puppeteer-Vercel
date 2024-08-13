24f1b5768d056d1261c6ce7f68683c70e2c4ca6d
pitieu <marcio1988@hotmail.com>
2024-08-13 13:08:49 +0800

    change to latest puppeteer

--- index.js
+++ index.js
@@ -41,53 +41,6 @@ app.get("/api", async (req, res) => {
   res.send(screenshot);
 });
 
-app.get("/api/video", async (req, res) => {
-  const url = req.query.url;
-  if (!url) {
-    return res.status(400).send("Bad Request: Please provide a URL.");
-  }
-
-  const browser = await playwright.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath(),
-    headless: true,
-  });
-
-  const context = await browser.newContext();
-  const page = await context.newPage();
-
-  await page.goto(url);
-
-  // Start video recording
-  await page.video().start({ width: 1280, height: 720 });
-
-  // Simulate scrolling
-  await page.evaluate(() => {
-    const totalHeight = document.body.scrollHeight;
-    let scrolled = 0;
-    const scrollStep = 100;
-    const scrollInterval = setInterval(() => {
-      window.scrollBy(0, scrollStep);
-      scrolled += scrollStep;
-      if (scrolled >= totalHeight) {
-        clearInterval(scrollInterval);
-      }
-    }, 100);
-  });
-
-  // Wait for scrolling to complete
-  await page.waitForTimeout(2000);
-
-  // Stop recording and get video
-  const video = await page.video().stop();
-
-  await browser.close();
-
-  // Send the video file
-  res.setHeader("Content-Type", "video/webm");
-  res.send(video);
-});
-
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started http://localhost:${process.env.PORT || 3000}`);
 });
