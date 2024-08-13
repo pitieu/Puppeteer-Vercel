1309222c1f5e08d45d78d5d8f01a354996033102
pitieu <marcio1988@hotmail.com>
2024-08-13 13:28:39 +0800

    c

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const app = require("express")();
 const cors = require("cors");
 const dotenv = require("dotenv");
-const chrome = require("@sparticuz/chromium");
+const chromium = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
 
 app.use(cors());
 
 dotenv.config();
+
+chromium.setHeadlessMode = true;
+chromium.setGraphicsMode = false;
 
 app.get("/api", async (req, res) => {
   const url = req.query.url;
@@ -15,16 +18,14 @@ app.get("/api", async (req, res) => {
     return res.status(400).send("Bad Request: Please provide a URL.");
   }
 
-  // if (process.env.SECRET !== req.query.secret)
-  //   throw new Error("Invalid secret");
-
   const browser = await puppeteer.launch({
-    args: chrome.args,
-    defaultViewport: chrome.defaultViewport,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-    ignoreHTTPSErrors: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+    headless: chromium.headless,
   });
+
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 630 });
   await page.goto(url, { waitUntil: "networkidle0" });
